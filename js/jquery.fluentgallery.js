a2965b170bf33f7d0d7a6bbe71bc10a1918fe7e2
jun <take.yapr@gmail.com>
2012-01-27 21:19:21 +0900

    protoype finished. but I needs add elements.

--- js/jquery.fluentgallery.js
+++ js/jquery.fluentgallery.js
@@ -30,13 +30,73 @@ THE SOFTWARE.
      * default Options
      */
     var defaults ={
-      is_quick         : true 
+      tile_class : ".tile" ,
+      resize     : false
     };
     
     var opts = $.extend(defaults , options);
     
     return this.each(function(){
-    	alert("")
+    	var area_elem = $(this);
+    	var total_width = area_elem.outerWidth(true);
+    	
+    	deployTiles(area_elem , opts.tile_class);
+    	
+    	if(opts.resize){
+	    	area_elem.bind("resize" , function(){
+	    		deployTiles(area_elem , opts.tile_class);
+	    	});
+	   }
     });
   };
+  
+	/**
+	 * Deploy tiles
+	 */
+	function deployTiles(area_elem , tile_class , column){
+		var is_first = false;
+		var total_column = 0;
+		var tile_column_width = 0;
+		var cols_height_array = new Array();
+
+		area_elem.find(tile_class).each(function(i , elem){
+			if(!is_first){
+				is_first = true;
+				tile_column_width = $(elem).outerWidth(true);
+				total_column = Math.floor(area_elem.outerWidth(true) / tile_column_width);
+				
+				//init array 
+				for(var i = 0; i < total_column; i++)cols_height_array.push(0);
+				
+				elem.style.top = '0px';
+				elem.style.left = '0px';
+			}
+			
+			var insert_position = getNextPosition(cols_height_array , elem , tile_column_width);
+			
+			area_elem.height(Math.max.apply(null, cols_height_array));
+			
+			elem.style.top  = insert_position.top + 'px';
+			elem.style.left = insert_position.left + 'px';
+			console.log(cols_height_array);
+		});
+	}
+	
+	/**
+	 * 次に配置すべき所を計算して配置する。
+	 */
+	function getNextPosition(cols_height_array , tile_elem , tile_column_width){
+		var length = cols_height_array.length;
+		var min = Math.min.apply(null, cols_height_array);  // → 1
+		console.log(min);
+		for(var i = 0; i <= length; i++){
+			if(min == cols_height_array[i]){
+				var insert_height = cols_height_array[i];
+				cols_height_array[i] += $(tile_elem).outerHeight();
+				return {left:tile_column_width*i , top:insert_height};
+			}
+		}
+		
+		return {top:0,left:0};
+	}
 })(jQuery);
